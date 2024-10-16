7397a249e9732e4a0c82e7ac983f98f67be2131a
ammyjutt <7mramir@gmail.com>
2024-10-16 05:09:55 +0500

    added new problem

---
+++ CPP_Online_Code/CPP_CODES_1/3213.cpp
@@ -0,0 +1,32 @@
+//using namespace std;
+//#include <iostream>
+//#include <vector>
+//#include <unordered_map>
+//
+//
+//class Solution {
+//public:
+//    int minimumCost(string target, vector<string>& words, vector<int>& costs) {
+//
+//       
+//        int len_words = words.size();
+//
+//        int len_target = target.length();
+//
+//        // take a map
+//        unordered_map<string, int> mp;
+//        for (int i = 0; i < len_words; i++)
+//        {
+//            mp[words[i]] = costs[i];
+//        }
+//
+//
+//
+//        vector< vector<int> > dp(len_target, vector<int>(len_target, -1));
+//
+//
+//    }
+//};
+//
+//
+//
